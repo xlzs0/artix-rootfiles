fd89fe1bf5fd932df1e4fd2a3f713c171e5062d7
xlzs0 <xlzs0.raptor@gmail.com>
2026-05-19 02:28:06 +0300

    2026-05-19 02:28

--- home/xlzs0/.config/micro/plug/tcc/tcc.lua
+++ home/xlzs0/.config/micro/plug/tcc/tcc.lua
@@ -1,7 +1,7 @@
 VERSION = "0.0.1"
 
 function onBufferOpen(buf)
-	if buf:Line(0):match("^#!.*tcc") then
+	if buf:Line(0):match("^#!.*tcc") or buf:Line(2):match("^exec tcc") then
 		buf:SetOptionNative("filetype", "c")
 	end
 end
